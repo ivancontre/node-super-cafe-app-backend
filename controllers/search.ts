1cde0ac1f6a9aeb9cb94e445cc610b0b4f67c7bb
Ivan Contreras <ivanc.contre@gmail.com>
2021-09-19 18:23:54 -0300

    ADD: Se agregan apis para busqueda por prodcto y categoruia. Ademas se agrega api para buscar productos por categoria

--- controllers/search.ts
+++ controllers/search.ts
@@ -1,15 +1,16 @@
 import { Request, Response} from 'express';
 import { Types } from 'mongoose';
-import { UserModel } from '../models';
+import { CategoryModel, ProductModel, UserModel } from '../models';
 
 const allowedCollections = [
     'users',
     'products',
     'categories',
-    'roles'
+    //'roles',
+    'products-by-category'
 ];
 
-const searchUser = async (term: string, res: Response) => {
+const searchUsers = async (term: string, res: Response) => {
 
     const isMongoId = Types.ObjectId.isValid(term);
 
@@ -37,8 +38,121 @@ const searchUser = async (term: string, res: Response) => {
         results: users
     });
 
+};
 
+const searchProducts = async (term: string, res: Response) => {
 
+    const isMongoId = Types.ObjectId.isValid(term);
+
+    if (isMongoId) {
+        const product = await ProductModel.findById(term).populate('category', 'name');
+
+        return res.status(200).json({
+            results: product ? [ product ] : []
+        });
+    }
+
+    const regex = new RegExp(term, 'i');
+
+    const products = await ProductModel.find({ 
+        $or: [
+            { name: regex }
+        ],
+        $and: [
+            { status: true }
+        ]
+     }).populate('category', 'name');
+
+    return res.status(200).json({
+        results: products
+    });
+};
+
+const searchCategories =  async (term: string, res: Response) => {
+
+    const isMongoId = Types.ObjectId.isValid(term);
+
+    if (isMongoId) {
+        const category = await CategoryModel.findById(term);
+
+        return res.status(200).json({
+            results: category ? [ category ] : []
+        });
+    }
+
+    const regex = new RegExp(term, 'i');
+
+    const categories = await CategoryModel.find({ 
+        $or: [
+            { name: regex }
+        ],
+        $and: [
+            { status: true }
+        ]
+     });
+
+    return res.status(200).json({
+        results: categories
+    });
+};
+
+const searchProductsByCategory = async (category: string, res: Response) => {
+
+    try {
+        
+        const isMongoId = Types.ObjectId.isValid(category);
+
+        if (isMongoId) {
+
+            const products = await ProductModel.find({ 
+                $or: [
+                    { category: new Types.ObjectId(category) }
+                ],
+                $and: [
+                    { status: true }
+                ]
+             }).populate('category', 'name');
+
+            return res.status(200).json({
+                results: products ? [ products ] : []
+            });
+        }
+
+        const regex = new RegExp(category, 'i');
+
+        const categories = await CategoryModel.find({ 
+            $or: [
+                { name: regex }
+            ],
+            $and: [
+                { status: true }
+            ]
+         });         
+
+        const products = await ProductModel.find({ 
+            $or: [
+                ...categories.map(category => {
+                    return {
+                        category: category._id
+                    }
+                })
+            ],
+            $and: [
+                { status: true }
+            ]
+        }).populate('category', 'name');
+
+        return res.status(200).json({
+            results: products
+        });
+        
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: 'Por favor hable con el administrador'
+        });
+    }
 };
 
 export const search = async (req: Request, res: Response) => {
@@ -56,18 +170,27 @@ export const search = async (req: Request, res: Response) => {
         switch (collection) {
             case 'users':
 
-                await searchUser(term, res);               
+                await searchUsers(term, res);               
                 
                 break;
 
             case 'products':
-                
+
+                await searchProducts(term, res);
+
                 break;
 
             case 'categories':
         
+                await searchCategories(term, res);
+
                 break;
 
+            case 'products-by-category':
+
+                await searchProductsByCategory(term, res);
+
+                break;
                 
             default:
 
